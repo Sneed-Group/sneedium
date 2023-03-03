20ee97836c897bce7ca7dd0557e425e26c249310
NodeMixaholic <sam@sllord.info>
2023-03-03 07:09:32 -0600

    Delete adblock.ts

--- adblock.ts
+++
@@ -1,6 +0,0 @@
-const fetchr = require("cross-fetch")
-const { ElectronBlocker, fullLists, Requests } = require('@cliqz/adblocker-electron')
-
-let mainWindow: BrowserWindow | null = null;
-let blocker = await ElectronBlocker.fromPrebuiltAdsAndTracking(fetchr);
-blocker.enableBlockingInSession(mainWindow.webContents.defaultSession);

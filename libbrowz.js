b664e3f3f108ed4a6dab78b5cbae4f36ea26a3d4
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-05-26 14:14:45 +0000

    fix google news.

--- libbrowz.js
+++ libbrowz.js
@@ -8,12 +8,12 @@ function go() {
     	url = url.replaceAll("youtu.be", "yewtu.be")
     } else if (url.includes("google.com/?q")) {
     	url = url.replaceAll("google.com/?q", "startpage.com/?q")
-    } else if (url.includes("google.com") && !url.includes("maps") && !url.includes("news") && !url.includes("webstore") && !url.includes("mail")) {
-    	url = url.replaceAll("google.com", "startpage.com")
     } else if (url.includes("https://news.google.com")) {
     	url = url.replaceAll("https://news.google.com", "http://68k.news")
     } else if (url.includes("news.google.com")) {
     	url = url.replaceAll("news.google.com", "68k.news")
+    } else if (url.includes("google.com") && !url.includes("maps") && !url.includes("news") && !url.includes("webstore") && !url.includes("mail")) {
+    	url = url.replaceAll("google.com", "startpage.com")
     }
     document.getElementById("txtUrl").value = ""
     browserFrame.loadURL(url);

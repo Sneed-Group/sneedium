38209621b936b3d1d0d8f056557dc5c03f788737
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-03 18:51:36 -0500

    Make the Mac maker make Mac binaries

--- sneedium-maker-mac.sh
+++ sneedium-maker-mac.sh
@@ -1,6 +1,6 @@
 y=$(date +%y)
 read -p "Sneedium4Mac # of year $y (1,2,3,4...):" v
-npm run make
+npm run make-mac
 mkdir "Sneedium $y.$v"
 mv sneedium-darwin-* "Sneedium4Mac $y.$v"
 tar -czvf "Sneedium4Mac $y.$v.tar.gz" "Sneedium4Mac $y.$v"

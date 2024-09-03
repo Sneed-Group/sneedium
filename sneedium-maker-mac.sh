1bef1f1615bf76b0816aac51a7d5eef6b67921de
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-03 18:47:16 -0500

    Add files via upload

---
+++ sneedium-maker-mac.sh
@@ -0,0 +1,9 @@
+y=$(date +%y)
+read -p "Sneedium4Mac # of year $y (1,2,3,4...):" v
+npm run make
+mkdir "Sneedium $y.$v"
+mv sneedium-darwin-* "Sneedium4Mac $y.$v"
+tar -czvf "Sneedium4Mac $y.$v.tar.gz" "Sneedium4Mac $y.$v"
+rm -rf "Sneedium $y.$v"
+mkdir ~/sneedium-mac-bins
+mv "Sneedium $y.$v.tar.gz" ~/sneedium-mac-bins

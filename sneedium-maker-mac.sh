010e954d7d6812c04e2bff0cc9753fa128980835
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-03 18:54:32 -0500

    Hotfix for the maker

--- sneedium-maker-mac.sh
+++ sneedium-maker-mac.sh
@@ -2,7 +2,8 @@ y=$(date +%y)
 read -p "Sneedium4Mac # of year $y (1,2,3,4...):" v
 npm run make-mac
 mkdir "Sneedium $y.$v"
-mv sneedium-darwin-* "Sneedium4Mac $y.$v"
+mv sneedium-darwin-arm64 "Sneedium4Mac $y.$v"
+mv sneedium-darwin-x64 "Sneedium4Mac $y.$v"
 tar -czvf "Sneedium4Mac $y.$v.tar.gz" "Sneedium4Mac $y.$v"
 rm -rf "Sneedium $y.$v"
 mkdir ~/sneedium-mac-bins

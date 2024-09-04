726d22f096e3b7dd932ab0ae36cb4c4550f270fb
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-03 19:10:29 -0500

    will it work?
    perhaps not

--- sneedium-maker-mac.sh
+++ sneedium-maker-mac.sh
@@ -2,9 +2,12 @@ y=$(date +%y)
 read -p "Sneedium4Mac # of year $y (1,2,3,4...):" v
 npm run make-mac
 mkdir "Sneedium $y.$v"
-mv sneedium-darwin-arm64 "Sneedium4Mac $y.$v"
-mv sneedium-darwin-x64 "Sneedium4Mac $y.$v"
+cp -r sneedium-darwin-arm64 "Sneedium4Mac $y.$v"
+cp -r sneedium-darwin-x64 "Sneedium4Mac $y.$v"
+rm -rf "Sneedium4Mac $y.$v/sneedium-darwin-arm64"
+rm -rf "Sneedium4Mac $y.$v/sneedium-darwin-x64"
 tar -czvf "Sneedium4Mac $y.$v.tar.gz" "Sneedium4Mac $y.$v"
 rm -rf "Sneedium4Mac $y.$v"
 mkdir ~/sneedium-mac-bins
-mv "Sneedium $y.$v.tar.gz" ~/sneedium-mac-bins
+cp "Sneedium $y.$v.tar.gz" "~/sneedium-mac-bins"
+rm "Sneedium $y.$v.tar.gz"

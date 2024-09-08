95219d729828272fc9603008b19fbddc46d41726
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-08 16:13:04 -0500

    demote mac maker to a shortcut

--- sneedium-maker-mac.sh
+++ sneedium-maker-mac.sh
@@ -1,13 +1,1 @@
-y=$(date +%y)
-read -p "Sneedium4Mac # of year $y (1,2,3,4...):" v
 npm run make-mac
-mkdir "Sneedium $y.$v"
-cp -r sneedium-darwin-arm64 "Sneedium4Mac $y.$v"
-cp -r sneedium-darwin-x64 "Sneedium4Mac $y.$v"
-rm -rf "sneedium-darwin-arm64"
-rm -rf "Ssneedium-darwin-x64"
-tar -czvf "Sneedium4Mac $y.$v.tar.gz" "Sneedium4Mac $y.$v"
-rm -rf "Sneedium4Mac $y.$v"
-mkdir ~/sneedium-mac-bins
-cp "Sneedium4Mac $y.$v.tar.gz" "~/sneedium-mac-bins"
-rm "Sneedium4Mac $y.$v.tar.gz"
